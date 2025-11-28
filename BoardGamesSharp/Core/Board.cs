88ca39f47216d32c2023c7025ad0a52a8149a1db
Daniil <shevdanil18@gmail.com>
2025-11-28 16:51:58 +0700

    Создание классов Dice, Cell, Board

---
+++ BoardGamesSharp/Core/Board.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGamesSharp.Core;
+
+public class Board
+{
+    private int rows;
+    private int columns;
+    private Cell[,] grid;
+
+    public Board(int row, int column) 
+    {  
+        rows = row; 
+        columns = column; 
+        grid = new Cell[rows, columns];
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns; c++)
+            {
+                grid[r, c] = new Cell();
+            }
+        }
+    }
+
+
+    public void Show()
+    {
+        for(int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns ; c++)
+            {
+                Console.Write($"{grid[r, c].Val,6}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public int GetValue(int row, int column)
+    {
+        if (row < 0 || row >= rows) return 0;
+        else if (column < 0 || column >= columns) return 0;
+        else return grid[row, column].Val;
+    }
+
+
+    public void SetValue(int row, int column, int val)
+    {
+        if (row >= 0 && row < rows)
+        if (column >= 0 && column < columns)
+        grid[row, column].Val = val;
+    }
+
+
+    public int PointSum(int column)
+    {
+        if (column < 0 || column >= columns) return 0;
+
+        int pointSum = 0;
+        for (int i = 0; i < rows - 1; i++)
+        {
+            pointSum += grid[i, column].Val;
+        }
+
+        grid[rows-1, column].Val = pointSum;
+        return pointSum;
+    }
+}
---
+++ BoardGamesSharp/Core/Cell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGamesSharp.Core;
+
+public class Cell(int v = 0)
+{
+    private int val = v;
+
+    public int Val
+    {
+        get { return val; }
+        set { val = value; }
+    }
+
+    public void Clear() { val = 0; }
+}
