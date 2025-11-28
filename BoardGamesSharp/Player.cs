7cad81385013d3c179c5197521aa9decfa579e8e
Daniil <shevdanil18@gmail.com>
2025-11-28 13:37:22 +0700

    Создание класса Player. Начало создания проекта на C#

---
+++ BoardGamesSharp/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGamesSharp.Core;
+
+public class Player
+{
+    private string name;
+    private int winCount;
+
+    public Player(string in_name)
+    {
+        name = in_name;
+        winCount = 0;
+    }
+
+    public void AddWin() {
+        winCount++;
+    }
+
+    public int Wins
+    {
+        get { return winCount; }
+        set { winCount = value; }
+    }   
+
+    public string Name
+    {
+        get { return name; }
+    }
+}
