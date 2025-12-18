7f9b1dc9bc52b71dac9baa599148939ced28dd5f
Daniil <shevdanil18@gmail.com>
2025-12-18 19:00:38 +0700

    Реализация клонирование класса Board. Конструктор копирования запрещен.

--- BoardGames/Board.cpp
+++ BoardGames/Board.cpp
@@ -4,7 +4,15 @@
 
 
 Board::Board(int row, int column) : rows(row), columns(column) {
-	grid.resize(rows, std::vector<Cell>(columns, Cell(0)));
+	// grid.resize(rows, std::vector<Cell>(columns, Cell(0)));
+
+	grid.resize(rows);
+	for (int i = 0; i < rows; ++i) {
+		grid[i].resize(columns);
+		for (int j = 0; j < columns; ++j) {
+			grid[i][j] = std::make_shared<Cell>(0);
+		}
+	}
 }
 
 Board::Board(const Board& saveState) : rows(saveState.rows), columns(saveState.columns), grid(saveState.grid) {}
@@ -12,7 +20,7 @@ Board::Board(const Board& saveState) : rows(saveState.rows), columns(saveState.c
 std::ostream& operator<<(std::ostream& os, const Board& board) {
 	for (int r = 0; r < board.rows; r++) {
 		for (int c = 0; c < board.columns; c++) {
-			os << std::setw(6) << board.grid[r][c].GetValue();
+			os << std::setw(6) << board.grid[r][c]->GetValue();
 		}
 		os << "\n";
 	}
@@ -23,14 +31,14 @@ std::ostream& operator<<(std::ostream& os, const Board& board) {
 int Board::GetValue(int row, int col) {
 	if (row < 0 || row > rows) return 0;
 	else if (col < 0 || col > columns) return 0;
-	else return grid[row][col].GetValue();
+	else return grid[row][col]->GetValue();
 }
 
 
 void Board::SetValue(int row, int col, int val) {
 	if (row >= 0 || row < rows)
 	if (col >= 0 || col < columns)
-	grid[row][col].SetValue(val);
+	grid[row][col]->SetValue(val);
 }
 
 
@@ -39,9 +47,26 @@ int Board::PointSum(int col) {
 
 	int pointSum = 0;
 	for (int i = 0; i < rows - 1; i++) {
-		pointSum += grid[i][col].GetValue();
+		pointSum += grid[i][col]->GetValue();
 	}
 
-	grid[rows-1][col].SetValue(pointSum);
+	grid[rows-1][col]->SetValue(pointSum);
 	return pointSum;
+}
+
+std::shared_ptr<Board> Board::ShallowClone() const {
+	return std::make_shared<Board>(*this);
+}
+
+
+std::shared_ptr<Board> Board::DeepClone() const {
+	auto clone = std::make_shared<Board>(rows, columns);
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < columns; ++j) {
+			clone->grid[i][j] = std::make_shared<Cell>(*grid[i][j]);
+		}
+	}
+
+	return clone;
 }
--- BoardGames/Board.hpp
+++ BoardGames/Board.hpp
@@ -7,13 +7,18 @@ class Board {
 private:
 	int rows;
 	int columns;
-	std::vector<std::vector<Cell>> grid;
+	std::vector<std::vector<std::shared_ptr<Cell>>> grid;
 public:
 	~Board() = default;
 
 	Board(int row, int column);
 
-	Board(const Board& saveState);
+	Board(const Board& saveState) = delete;
+	Board& operator=(const Board&) = delete;
+
+	std::shared_ptr<Board> ShallowClone() const;
+	std::shared_ptr<Board> DeepClone() const;
+
 	
 	int GetValue(int row, int col);
 	void SetValue(int row, int col, int value);
